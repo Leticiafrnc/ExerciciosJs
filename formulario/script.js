8197e38536f9addec381d4a03356d3911631af19
Letícia Franco <leticiafrnc1@gmail.com>
2025-05-23 14:52:49 -0300

    update formulario

---
+++ formulario/script.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("form")
+const nome = document.querySelector("#nome")
+const email = document.querySelector("#email")
+const assunto = document.querySelector("#assunto")
+const mensagem = document.querySelector("#mensagem")
+const errorMessages = document.querySelectorAll(".error-message")
+
+// ao cliar no input não envio os dados
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validateInputs();
+});
+
+// função para o erro
+
+function setError(input, errorMessage){
+    const errorMessageElement = input.nextElementSibling
+    errorMessageElement.textContent = errorMessage
+    //deixar a caixa vermelha 
+    input.parentElement.classList.add("error")
+}
+
+// validação dos campos 
+function validateInputs() {
+    const nomeValue = nome.value.trim()
+    const emailValue = email.value.trim()
+    const assuntoValue = assunto.value.trim()
+    const mensagemValue = mensagem.value.trim()
+
+
+    if(nomeValue === ""){
+        setError(nome, "Preencha o nome")
+    }
+    if(emailValue === ""){
+        setError(email, "Preencha o email")
+    }
+    if(assuntoValue === ""){
+        setError(assunto, "Preencha o assunto")
+    }
+    if(mensagemValue === ""){
+        setError(mensagem, "Preencha a mensagem")
+    }
+}
